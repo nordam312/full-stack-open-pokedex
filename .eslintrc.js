55b66c39d969c107f465915ed810ab9b83f7bd74
nordam312 <nordam312@gmail.com>
2026-01-19 23:41:27 +0300

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,17 +18,16 @@ module.exports = {
     indent: ["error", 2],
     "linebreak-style": "off",
     quotes: "off",
-    semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
     "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": "error",
     "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
   },
   settings: {
     react: {
       version: "detect",
     },
   },
-}
+};
